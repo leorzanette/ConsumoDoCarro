fa9f70268f085f19ca719584ed3b43ba1d5352b4
Leonardo <leorzanette@gmail.com>
2026-01-23 15:44:57 -0300

    First successful PR, added to build.gradle.kts(:app): plugins {   id("com.google.devtools.ksp") } kotlin {   compilerOptions{     jvmTarget.set(JvmTarget.JVM_17)   } } android {   compileOptions {     sourceCompatibility = JavaVersion.VERSION_17     targetCompatibility = JavaVersion.VERSION_17   } } dependencies {   val room_version = "2.8.4"   implementation("androidx.room:room-runtime:$room_version")   implementation("androidx.room:room-ktx:${room_version}")   ksp("androidx.room:room-compiler:$room_version") } Added to build.gradle.kts(Consumo do Carro): plugins {   id("com.google.devtools.ksp") version "2.3.4" apply false } Changed the text in MainActivity.kt: fun Greeting() {     Text(         text = "Hello $name! It's working with Room and KSP!!",     ) } Sync worked flawlessly.

--- app/src/main/java/com/lelular/consumodocarro/MainActivity.kt
+++ app/src/main/java/com/lelular/consumodocarro/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name! It's working with Room and KSP!!",
         modifier = modifier
     )
 }
